c052620c0fd8c14d91845edaef6880fec3269a0a
sdi2200288 <altanitontou1@gmail.com>
2025-12-20 13:55:01 +0200

    side menu arxikes

--- src/pages/HomePage/OwnerDashboard.js
+++ src/pages/HomePage/OwnerDashboard.js
@@ -3,23 +3,74 @@ import Footer from "../../components/Footer/Footer";
 import ArxikiOwner from "../../images/ArxikiOwner.png";
 import "./OwnerDashboard.css";
 import { Link } from "react-router-dom";
+import {
+  FaUser,
+  FaFileAlt,
+  FaExclamationCircle,
+  FaBook,
+  FaCalendarAlt,
+  FaHistory
+} from "react-icons/fa";
+
 
 export default function OwnerDashboard() {
   return (
     <div className="OwnerDashboard">
-      <section className="hero-section">
-        <img src={ArxikiOwner} alt="ArxikiOwner" className="main-image" />
-      </section>
+      
       <nav className="breadcrumb">
         <Link to="/">Αρχική /</Link>
         <span>Dashboard Ιδιοκτήτη</span>
       </nav>
 
-      <div className="dashboard-content">
-        
-      </div>
+      <div className="dashboard-layout">
+        <aside className="sidebar">
+          <h3>Επιλογές Ιδιοκτήτη</h3>
+      
+          <ul className="sidebar-menu">
+            <li>
+              <Link to="/owner/profile">
+                <FaUser className="menu-icon" /> <span>Το προφίλ μου</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/owner/statement-finding">
+                <FaFileAlt className="menu-icon" /> <span>Δηλώσεις Εύρεσης</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/owner/loss-declaration">
+                <FaExclamationCircle className="menu-icon" /> <span>Δηλώσεις Απώλειας</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/owner/health-booklet">
+                <FaBook className="menu-icon" /> <span>Προβολή Βιβλιαρίου</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/owner/bookings">
+                <FaCalendarAlt className="menu-icon" /> <span>Κλείσιμο Ραντεβού</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/owner/history-bookings">
+                <FaHistory className="menu-icon" /> <span>Ιστορικό Ραντεβού</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/owner/future-bookings">
+                <FaCalendarAlt className="menu-icon" /> <span>Μελλοντικά Ραντεβού</span>
+              </Link>
+            </li>
+          </ul>
+        </aside>
+    
     </div>
+  </div>
 
   );
 }
+
+
+
 
--- src/pages/HomePage/VetDashboard.js
+++ src/pages/HomePage/VetDashboard.js
@@ -1,25 +1,107 @@
-import React from "react";
+import React,{useState} from "react";
 import Footer from "../../components/Footer/Footer";
 import ArxikiVet from "../../images/ArxikiVet.png";
 import "./VetDashboard.css";
 import { Link } from "react-router-dom";
+import {
+  FaUser,
+  FaFileAlt,
+  FaExclamationCircle,
+  FaBook,
+  FaCalendarAlt,
+  FaHistory
+} from "react-icons/fa";
+
 
 export default function VetDashboard() {
+  const [openDeclarations, setOpenDeclarations] = useState(false);
+  const [openAnimalRegister, setOpenAnimalRegister] = useState(false);
   return (
     <div className="VetDashboard">
-      <section className="hero-section">
-        <img src={ArxikiVet} alt="ArxikiVet" className="main-image" />
-      </section>
+      
       <nav className="breadcrumb">
-        <Link to="/">Αρχική /</Link>
+        <Link to="/">Αρχική</Link>
+        <span className="separator"> &gt; </span>
         <span>Dashboard Κτηνίατρου</span>
       </nav>
 
-      <div className="dashboard-content">
-        
-      </div>
+      <div className="dashboard-layout">
+        <aside className="sidebar">
+          <h3>Επιλογές Κτηνίατρου</h3>
+      
+          <ul className="sidebar-menu">
+            <li>
+              <Link to="/owner/profile">
+                <FaUser className="menu-icon" /> <span>Το προφίλ μου</span>
+              </Link>
+            </li>
+            <li>
+              <button className="menu-button" onClick={() => setOpenDeclarations(!openDeclarations)}>
+                <FaFileAlt className="menu-icon" />
+                <span>Δηλώσεις</span>
+                <span className={`arrow ${openDeclarations ? "open" : ""}`}>▾</span>
+              </button>
+              {openDeclarations && (
+                <ul className="submenu">
+                  <li>
+                    <Link to="/vet/declarations/finding"> Δήλωση Εύρεσης </Link>
+                  </li>
+                  <li>
+                    <Link to="/vet/declarations/loss"> Δήλωση Απώλειας </Link>
+                  </li>
+                  <li>
+                    <Link to="/vet/declarations/import"> Δήλωση Υιοθεσίας </Link>
+                  </li>
+                  <li>
+                    <Link to="/vet/declarations/import"> Δήλωση Αναδοχής </Link>
+                  </li>
+                  <li>
+                    <Link to="/vet/declarations/import"> Δήλωση Μεταβίβασης </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+             <li>
+              <button className="menu-button" onClick={() => setOpenAnimalRegister(!openAnimalRegister)}>
+                <FaFileAlt className="menu-icon" />
+                <span>Καταγραφή Ζώου</span>
+                <span className={`arrow ${openAnimalRegister ? "open" : ""}`}>▾</span>
+              </button>
+              {openAnimalRegister && (
+                <ul className="submenu">
+                  <li>
+                    <Link to="/vet/declarations/finding"> Καταγραφή Ταυτότητας </Link>
+                  </li>
+                  <li>
+                    <Link to="/vet/declarations/loss"> Ενημέρωση Ιατρικών Πράξεων </Link>
+                  </li>
+                  <li>
+                    <Link to="/vet/declarations/import"> Προβολή Βιβλιαρίου</Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+
+            <li>
+              <Link to="/owner/bookings">
+                <FaCalendarAlt className="menu-icon" /> <span>Ενημέρωση Διαθεσιμότητας</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/owner/history-bookings">
+                <FaHistory className="menu-icon" /> <span>Ιστορικό Ραντεβού</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/owner/future-bookings">
+                <FaCalendarAlt className="menu-icon" /> <span>Μελλοντικά Ραντεβού</span>
+              </Link>
+            </li>
+          </ul>
+        </aside>
+    
     </div>
+  </div>
 
   );
 }
-
