02075de99d4d39dce305d119290aeb424ce8f126
sdi2200288 <altanitontou1@gmail.com>
2026-01-14 12:04:10 +0200

    db

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -10,6 +10,7 @@ import "./HomePage.css";
 import { Link } from "react-router-dom";
 import { formatNumber } from "../Utils/Util";
 import "../OwnerDashboard/BookDate";
+import vetdefault from "../../images/vet.png";
 
 export default function Homepage() {
   const [ownersData, setOwnersData] = useState([]);
@@ -201,10 +202,9 @@ export default function Homepage() {
                       }}
                     >
                       <div className="vet-image">
-                        <img className="pet-img" src={v.photoUrl ||'https://tse3.mm.bing.net/th/id/OIP.PPPpZTMMeoaSphabYUqpGQHaE8?w=799&h=533&rs=1&pid=ImgDetMain&o=7&rm=3'}
-                          alt={v.lastname}
+                        <img src={v.photoUrl || vetdefault}
                           onError={(e) => {
-                            e.target.src = "https://th.bing.com/th/id/OIP.H1gHhKVbteqm1U5SrwpPgwHaFj?w=265&h=199&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3";
+                            e.target.src = vetdefault;
                           }} />
                       </div>
 
