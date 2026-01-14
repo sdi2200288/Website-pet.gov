7bf01917e7192b1bdcefaddddd0ddd2da632771e
sdi2200006 <sdi2200006@uoa.gr>
2026-01-14 16:51:33 +0200

    db

--- src/pages/OwnerDashboard/BookDate.js
+++ src/pages/OwnerDashboard/BookDate.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import "./BookDate.css";
-import "../../components/Vet/Vet";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { FiSearch, FiUser, FiMapPin, FiCalendar, FiClock, } from "react-icons/fi";
